ddaae6e1e56589e931838e49cb822f3c23bb6eff
Mr.Anoy <adjustmode1@gmail.com>
2022-06-14 16:54:49 +0700

    dcoker

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:16.15-alpine3.16
+
+WORKDIR /usr/app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 8000
+
+CMD ["npm","run","start:dev"]
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports:[MongooseModule.forRoot('mongodb://thanhhuy:thanhhuy@localhost:27017/nest_mongo')]
-    // imports:[MongooseModule.forRoot('mongodb+srv://thanhhuy:thanhhuy@cluster0.a0gzx.mongodb.net/?retryWrites=true&w=majority')]
+    // imports:[MongooseModule.forRoot('mongodb://thanhhuy:thanhhuy@localhost:27017/nest_mongo')]
+    imports:[MongooseModule.forRoot('mongodb+srv://thanhhuy:thanhhuy@cluster0.a0gzx.mongodb.net/?retryWrites=true&w=majority')]
 })
 export class DatabaseModule {}
