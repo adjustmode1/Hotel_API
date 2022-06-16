3ff780df6b99828f9ef47f228011e327a61df96b
Mr.Anoy <adjustmode1@gmail.com>
2022-06-16 11:03:23 +0700

    test

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
     // imports:[MongooseModule.forRoot('mongodb://thanhhuy:thanhhuy@localhost:27017/nest_mongo')]
-    imports:[MongooseModule.forRoot('mongodb+srv://thanhhuy:thanhhuy@cluster0.a0gzx.mongodb.net/?retryWrites=true&w=majority')]
+    imports:[MongooseModule.forRoot('mongodb://localhost:6789/local')]
+    // imports:[MongooseModule.forRoot('mongodb+srv://thanhhuy:thanhhuy@cluster0.a0gzx.mongodb.net/?retryWrites=true&w=majority')]
 })
 export class DatabaseModule {}
