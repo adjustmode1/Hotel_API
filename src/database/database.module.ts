8c735bbd44288833da4668a77d22132e6b3ca583
Mr.Anoy <adjustmode1@gmail.com>
2022-06-16 14:36:34 +0700

    final

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,7 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
     // imports:[MongooseModule.forRoot('mongodb://thanhhuy:thanhhuy@localhost:27017/nest_mongo')]
-    imports:[MongooseModule.forRoot('mongodb://localhost:6789/local')]
+    imports:[MongooseModule.forRoot('mongodb://db_api_mongodb:27017/local')]
     // imports:[MongooseModule.forRoot('mongodb+srv://thanhhuy:thanhhuy@cluster0.a0gzx.mongodb.net/?retryWrites=true&w=majority')]
 })
 export class DatabaseModule {}
