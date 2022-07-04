fde9273360650513986884bf9b23a4ff1521ffec
Mr.Anoy <adjustmode1@gmail.com>
2022-07-04 09:53:03 +0700

    test

--- src/type-room-api/dto/update-type-room-api.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTypeRoomApiDto } from './create-type-room-api.dto';
-
-export class UpdateTypeRoomApiDto extends PartialType(CreateTypeRoomApiDto) {}
